8ff4b4a2ab007ef331f0eb3bdebec1c2ccd501b2
Anup Adhikari <anupadhikari574@gmail.com>
2023-09-05 12:14:52 +0545

    Update DAY 7 - Hangman.py
    minor change to the code: strings and hagman more creative

--- DAY 7 - Hangman/DAY 7 - Hangman.py
+++ DAY 7 - Hangman/DAY 7 - Hangman.py
@@ -1,31 +1,57 @@
 # step1
-word_list = ["hangman", "apple", "programming", "peoaple","beea","amississippi"]
 
 import random
+from hangman_word import word_list
+from hangman_art import logo, stages
 
+print(f"\nInstructions:\nYou have 10 Lives to guess correct word! \nHope You will Enjoy Playing. ")
+
+# word_list = ["hangman", "apple", "programming", "peoaple","beea","amississippi"]
+# word_list = ['hangman', 'apple', 'programming', 'people', 'bee', 'mississippi']
+
+# choosen_word = random.choice(hangman_word.word_list)
 choosen_word = random.choice(word_list)
 word_length = len(choosen_word)
 ending = False
 display = []
-lives = 6
+lives = 9
+
+print(logo)
 for _ in range(word_length):
   display += "_"
-  
+print(f"Pssst, the solution is {choosen_word}")
+
 while not ending:
   user_guess = input("Enter a letter to guess: ").lower()
-  
+
+  if user_guess in display:
+    print(f"Awww! you have already enter {user_guess}")
+
+  # watch FOR IN loop and String working with for loop:
+
   for position in range(word_length):
     letter = choosen_word[position]
     if letter == user_guess:
-      display[position] += user_guess
-      
-  print (display)
+      display[position] = user_guess
+
+  # print(display)
+  # converting following into string's
+  print(f"{' '.join(display)}")
+  
+  
+  
+
   if user_guess not in choosen_word:
+    print(f"\nOop! You loose a point, {user_guess} letter isn't in a word ")
+    
     lives -= 1
     if lives == 0:
       ending = True
-      print(f"Game Over! \n Word is {choosen_word}")
+      print("Game Over!")
 
   if "_" not in display:
     ending = True
-    print(f"You Win!\nWord is {choosen_word}")
+    print(f"You Win!\nword is {choosen_word}")
+
+  pic = stages[lives]
+  print(pic)
